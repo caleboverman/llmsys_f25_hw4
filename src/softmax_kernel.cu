35d747beeecee8ff32cfbcc72f8455b95f9cbb4d
caleboverman <caleboverman@gmail.com>
2025-10-15 20:34:18 -0500

    4.1.1

--- src/softmax_kernel.cu
+++ src/softmax_kernel.cu
@@ -150,7 +150,27 @@ __global__ void ker_attn_softmax(T *inp, const T *attn_mask, int from_len,
     /* step 1. compute max */
     // thread local max
     // BEGIN ASSIGN4_1_1
-    
+    float val[token_per_reduce][ele_per_thread];
+    float l_max[token_per_reduce];
+    for (int i = 0; i < token_per_reduce; ++i) {
+      l_max[i] = REDUCE_FLOAT_INF_NEG;
+      for (int j = 0; j < ele_per_thread; ++j) {
+        int col = threadIdx.x * ele_per_thread + j;
+        float temp_val = REDUCE_FLOAT_INF_NEG;
+        if (col < to_len) {
+          if (mask_future && col > token_id + i) {
+            temp_val = REDUCE_FLOAT_INF_NEG;
+          } else {
+            temp_val = static_cast<float>(inp_val[i][j]);
+            if (attn_mask) {
+              temp_val += static_cast<float>(mval[j]);
+            }
+          }
+        }
+        val[i][j] = temp_val;
+        l_max[i] = fmaxf(l_max[i], temp_val);
+      }
+    }
     // END ASSIGN4_1_1
     // block reduce max
     blockReduce<ReduceType::kMax, token_per_reduce>(l_max);
@@ -166,7 +186,22 @@ __global__ void ker_attn_softmax(T *inp, const T *attn_mask, int from_len,
     /* step 2. compute sum */
     // thread local sum
     // BEGIN ASSIGN4_1_1
-    
+    float l_sum[token_per_reduce];
+    for (int i = 0; i < token_per_reduce; ++i) {
+      l_sum[i] = 0.f;
+      float max_val = s_max[i];
+      for (int j = 0; j < ele_per_thread; ++j) {
+        int col = threadIdx.x * ele_per_thread + j;
+        float exp_val = 0.f;
+        if (col < to_len) {
+          exp_val = __expf(val[i][j] - max_val);
+          val[i][j] = exp_val;
+          l_sum[i] += exp_val;
+        } else {
+          val[i][j] = 0.f;
+        }
+      }
+    }
     // END ASSIGN4_1_1
     // block reduce sum
     blockReduce<ReduceType::kSum, token_per_reduce>(l_sum);
@@ -181,7 +216,17 @@ __global__ void ker_attn_softmax(T *inp, const T *attn_mask, int from_len,
 
     /* step 3. compute final result */
     // BEGIN ASSIGN4_1_1
-   
+    for (int i = 0; i < token_per_reduce && (token_id + i) < from_len; ++i) {
+      float inv_sum = s_sum[i];
+      for (int j = 0; j < ele_per_thread; ++j) {
+        int col = threadIdx.x * ele_per_thread + j;
+        if (col < to_len) {
+          inp_val[i][j] = (T)(val[i][j] * inv_sum);
+        }
+      }
+      BlockStore(ts_store).Store(inp + (token_id + i) * to_len, inp_val[i], to_len,
+                                 REDUCE_FLOAT_INF_NEG);
+    }
     // END ASSIGN4_1_1
   }  // blockIdx.x
 }
